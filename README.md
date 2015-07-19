147ba78fb5953661759978dad8381675aea131aa
valerio-vaccaro <valerio.vaccaro@gmail.com>
2015-07-19 23:59:25 +0200

    working on send feature

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # node-red-contrib-phant
-Node-red node for upload tuples of data on a phant storage service
+A [Node-RED](http://nodered.org) nodes for upload tuples of data on a phant storage service.
+
+## Install
+Use npm to command to install this package locally in the Node-RED modules directory
+```bash
+npm install node-red-contrib-phant 
+```
+or install in it globally with the command
+```bash
+npm install node-red-contrib-phant -g 
+```
+
+## Nodes included in the package
+** ** 
+
+## Usage example
+![Basic usage](https://github.com/valerio-vaccaro/node-red-contrib-bleacon/blob/master/node-red-contrib-bleacon_example.png)
+Simple usage of the plugin in Node-RED, a message with ON or 1 will turn on the node, otherwise a message with OFF or 0 will turn off the node.
+```json
+[
+
+]
+```
+
+## History
+- 0.0.1 - June 2015 : Initial Release
+
+## Authors
+* Valerio Vaccaro (https://github.com/valerio-vaccaro)
+
+## Credits
+Node-RED has been made possible by the hard work of Nick O'Leary @knolleary and Dave Conway-Jones @ceejay at IBM Emerging Technology. Much thanks to them and other supporters for advancing this platform. 
+This module is developed by Valerio Vaccaro (http://www.valeriovaccaro.it) on Raspberry Pi embedded board.
+
+## License
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ phant/phant.js
@@ -0,0 +1,76 @@
+/**
+ * phant.js
+ * Phant client node
+ * Requires phant-client: https://github.com/dpjanes/iotdb-phant-client
+ * Copyright 2015 Valerio Vaccaro - www.valeriovaccaro.it
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+var RED = require(process.env.NODE_RED_HOME+"/red/red");
+
+var Phant = require('phant-client').Phant
+var phant = new Phant()
+
+function PhantClient(n) {
+    RED.nodes.createNode(this,n);
+
+    var msg = {};
+    var publicKey;
+	var privateKey;
+	var deleteKey;
+    var node = this;
+
+
+    // Get varables from the node
+    this.publicKey = n.publicKey;
+    this.privateKey = n.privateKey;
+	this.deleteKey = n.deleteKey;
+
+    var streamd = {
+        manageUrl: "https://data.sparkfun.com/streams/"+this.publicKey,
+        outputUrl: "https://data.sparkfun.com/output/"+this.publicKey,
+        inputUrl: "https://data.sparkfun.com/input/"+this.publicKey,
+        publicKey: this.publicKey,
+        privateKey: this.privateKey,
+        deleteKey: this.deleteKey
+    }
+
+    // Status icon
+    this.status({fill:"grey",shape:"dot",text:"not scanning"});
+
+    this.on("input", function(msg){
+        phant.connect(streamd, function(error, streamd) {
+            if (error) {
+                this.status({fill:"red",shape:"dot",text:"Error in connection"});
+            } else {
+                this.status({fill:"green",shape:"dot",text:"Connected"});
+                //phant.save("stream.json", streamd)
+                phant.add(streamd, msg.payload , function(error, rd) {
+                    if (error == null)
+                         this.status({fill:"green",shape:"dot",text:"Published"});
+                    else
+                        this.status({fill:"red",shape:"dot",text:error});
+                })
+            }
+        })
+    });
+
+    this.on("close", function() {
+
+    });
+}
+
+// Register the node by name. This must be called before overriding any of the
+// Node functions.
+RED.nodes.registerType("phant", PhantClient);
